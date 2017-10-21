299d1071a0b789c3860c1c382c684e5c3381f0a1
martins92 <martinsratkus92@gmail.com>
2017-10-21 13:04:02 +0300

    added some CSS
    created header of the app

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/* ====================================
+              VARIABLES
+   ==================================== */
+
+// const avatarImage = document.querySelector('.avatar__main');
+// const logo = document.querySelector('.header-logo__main');
+// const name = document.createElement('p');
+
+/* ====================================
+              FUNCTIONS
+   ==================================== */
+
+// funciton removes logo image once avatar image is clicked
+
+// const logoRemove = function() {
+//   logo.classList.remove('header-logo__main');
+//   logo.classList.add('hidden');
+// }
+
+// const addRightMargin = function() {
+//   name.textContent = 'Fullname';
+//   avatarImage.appendChild(name);
+//   name.classList.add('avatar__main--name');
+// }
+
+// const addRemove = function() {
+//   logo.classList.remove('hidden');
+//   logo.classList.add('header-logo__main');
+// }
+
+
+
+/* ====================================
+              EVENTS
+   ==================================== */
+
+// listens for avatar image click events
+
+// avatarImage.addEventListener('click', ()=> {
+//   addRightMargin();
+//   setTimeout(logoRemove, 3000);
+// });
